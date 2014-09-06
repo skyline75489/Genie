836ebdbb3a58014b290e8443ce988d237c6fe91d
skyline75489 <skyline75489@outlook.com>
2014-09-06 09:21:59 +0800

    add gitignore

--- genie.py
+++ genie.py
@@ -2,8 +2,12 @@ import os
 import time
 import codecs
 import json
+import logging
+
 import misaka
 
+
+logger = logging.getLogger()
 
 class Post(object):
 
@@ -28,7 +32,9 @@ class Post(object):
 
 
 class Genie(object):
-
+    
+    MAX_POSTS_PER_PAGE = 15
+    
     def __init__(self):
         """Initialize misaka Markdown parser 
         and settings
@@ -49,22 +55,27 @@ class Genie(object):
         self.in_file_path = settings['in_file_path']
         self.out_file_path = settings['out_file_path']
         self.blog_name = settings['blog_name']
-
-    def _generate_index(self):
+        
+    def _generate_page_with(self, posts, out_file, current_page=1):
         post_titles = ""
-        for post in self.posts:
+        for post in posts:
             # The About page, ignore it
             if post.file_name == "about":
                 continue
-            post_titles += '<a class="title" href="' + post.dst_name + '">' + \
-                post.title + '</a><span>' + post.mtime + '</span><br>\n'
+            post_titles += '<div class="entry"><a class="title" href="' + post.dst_name + '">' + \
+                post.title + '</a><span class="date">' + post.mtime + '</span></div>\n'
 
         result = self.index_template.format(
-            content=post_titles, blog_name=self.blog_name).encode('utf-8')
+            content=post_titles, blog_name=self.blog_name, bottom_nav="").encode('utf-8')
 
-        fout = open(self.out_file_path + "index.html", "w")
+        fout = open(self.out_file_path + out_file, "w")
         fout.write(result)
         fout.close()
+        
+    def _generate_index(self):
+        start = 0
+        end = self.MAX_POSTS_PER_PAGE
+        self._generate_page_with(self.posts, 'index.html')
 
     def _generate_post(self, text, out_file_name):
         html = misaka.html(text, extensions=self.ext)
@@ -74,8 +85,11 @@ class Genie(object):
         fout = open(out_file_name, "w")
         fout.write(result)
         fout.close()
-
-    def _read_template_files(self):
+        
+    def _generate_archive(self):
+        pass
+        
+    def _get_templates(self):
         blog_template_file = codecs.open(
             "./templates/post_template.html", mode="r", encoding="utf8")
         index_template_file = codecs.open(
@@ -85,7 +99,7 @@ class Genie(object):
         blog_template_file.close()
         index_template_file.close()
 
-    def _read_post_files(self):
+    def _get_posts(self):
 
         src_path = self.in_file_path
         dst_path = self.out_file_path
@@ -98,11 +112,8 @@ class Genie(object):
                 if part[1] in [".md", ".markdown"]:
                     post = Post(src_path, dst_path, part[0], part[1])
                     self.posts.append(post)
-
-    def update(self):
-        self._read_template_files()
-        self._read_post_files()
-
+                    
+    def _render(self):
         # Sort post in last-modified time ascending
         self.posts.sort(lambda p1, p2: cmp(p2.mtime_unix, p1.mtime_unix))
         # Generate html for every post we have
@@ -111,6 +122,11 @@ class Genie(object):
             self._generate_post(raw_text, post.dst_full_path)
 
         self._generate_index()
+                
+    def update(self):
+        self._get_templates()
+        self._get_posts()
+        self._render()
 
 if __name__ == '__main__':
     g = Genie()
